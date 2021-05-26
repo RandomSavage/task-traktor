2d0a020f530d296a0588633ebb5cdac0f673a5c5
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2021-05-25 21:01:48 -0400

    Add tasks/bugs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 
 
@@ -28,6 +29,12 @@ function App() {
       reminder: false
     },
   ])
+  //Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
 
   //Delete Task
   const deleteTask = (id) => {
@@ -43,6 +50,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks to show'}
       {/* <h1>Task_Traktor</h1> */}
       {/* <h2>Hello {name}!</h2> */}
